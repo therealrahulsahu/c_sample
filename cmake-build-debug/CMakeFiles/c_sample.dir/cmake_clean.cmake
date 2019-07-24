00c5c5a0352e0a3354f5e89f7421a8e4f6ac6ebe
therealrahulsahu <43601158+therealrahulsahu@users.noreply.github.com>
2019-07-25 03:18:48 +0530

    practice

---
+++ cmake-build-debug/CMakeFiles/c_sample.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/c_sample.dir/main.c.obj"
+  "c_sample.pdb"
+  "c_sample.exe"
+  "c_sample.exe.manifest"
+  "libc_sample.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/c_sample.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
