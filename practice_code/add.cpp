9921e9ba2d8ce9716ffb75778e1f6be7793ae014
therealrahulsahu <43601158+therealrahulsahu@users.noreply.github.com>
2019-07-30 22:50:20 +0530

    practice

--- practice_code/add.cpp
+++ practice_code/add.cpp
@@ -4,7 +4,12 @@ int add(int a=0, int b=0)
 {
     return a + b;
 }
+int mul(int a=0, int b=0)
+{
+    return a*b;
+}
 int main()
 {
-    cout<<"Sum : "<<add(5,6);
+    cout<<"Sum : "<<add(5,6)<<endl;
+    cout<<"Mul : "<<mul(4,7);
 }
