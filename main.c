4cb08e5da57a6e4c3a4e43fc6fa4a43819477ff1
therealrahulsahu <43601158+therealrahulsahu@users.noreply.github.com>
2019-08-12 03:08:25 +0530

    s&s project submission

--- main.c
+++ main.c
@@ -2,8 +2,8 @@
 
 int dayofweek(int d, int m, int y)
 {
-    static int t[] = { 0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4 };
-    y -= m < 3;
+    static int t[] = { 6, 2, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4 };
+    //y -= m < 3;
     return ( y + y/4 - y/100 + y/400 + t[m-1] + d) % 7;
 }
 
@@ -11,7 +11,7 @@ int dayofweek(int d, int m, int y)
 int main()
 {
     char *name[]={ "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" };
-    int day = dayofweek(12, 8, 2019);
+    int day = dayofweek(1, 1, 1800);
     printf ("%s", name[day]);
     return 0;
 }
--- special/dayofweek.c
+++ special/dayofweek.c
@@ -6,12 +6,19 @@ int dayofweek(int d, int m, int y)
     y -= m < 3;
     return ( y + y/4 - y/100 + y/400 + t[m-1] + d) % 7;
 }
+int dayofweek_modified(int d, int m, int y)
+{
+    static int t[] = { 6, 2, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4 };
+    //y -= m < 3;
+    return ( y + y/4 - y/100 + y/400 + t[m-1] + d) % 7;
+}
 
 /* Driver function to test above function */
 int main()
 {
     char *name[]={ "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" };
-    int day = dayofweek(12, 8, 2019);
+    //int day = dayofweek(12, 8, 2019);
+    int day = dayofweek_modified(12, 8, 2019);
     printf ("%s", name[day]);
     return 0;
 }
