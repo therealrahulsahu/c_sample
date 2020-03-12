7dcbda6d1ae92f7b4dee6d9a0a40515b20b79317
Rahul Sahu <43601158+therealrahulsahu@users.noreply.github.com>
2020-03-12 17:13:11 +0530

    Update 12mar.c

--- cg_lab/12mar.c
+++ cg_lab/12mar.c
@@ -1,6 +1,6 @@
 #include<graphics.h>
 #include<math.h>
-void multiply(int A[3][3], int B[3][3], int C[3][3]) 
+void multiply(float A[3][3], float B[3][3], float C[3][3]) 
 { 
 	int i, j, k;
     for (i = 0; i < 3; i++) 
@@ -16,8 +16,8 @@ void multiply(int A[3][3], int B[3][3], int C[3][3])
     } 
 } 
 void main(){
-	int gd=DETECT, gm, type, trans, tx, ty, angle, sx, sy, valid=1, x1, y1, x2, y2, rad, row, col;
-	int t_mat[3][3]={0}, data[3][3]={0}, result[3][3]={0};
+	int gd=DETECT, gm, type, trans, tx, ty, angle, sx, sy, valid=1, x1, y1, x2, y2, rad;
+	float t_mat[3][3]={0}, data[3][3]={0}, result[3][3]={0};
 	initgraph(&gd, &gm, "c:\\turboc3\\bgi");
 	printf("1 :Line\n2 :Rectangle\n3 :Circle\nEnter : ");
 	scanf("%d", &type);
@@ -50,7 +50,7 @@ void main(){
 		data[2][1] = 1;
 	}else if(type==3){
 		printf("Enter x, y and radius : ");
-		scanf("%d%d", &x1, &y1, &rad);
+		scanf("%d%d%d", &x1, &y1, &rad);
 		cleardevice();
 		circle(x1, y1, rad);
 		data[0][0] = x1;
@@ -73,8 +73,15 @@ void main(){
 		}else if(trans==2){
 			printf("Enter Angle : ");
 			scanf("%d", &angle);
+			t_mat[0][0] = cos(angle);
+			t_mat[1][0] = sin(angle);
+			t_mat[2][0] = 0;
+			t_mat[0][1] = -1*sin(angle);
+			t_mat[1][1] = cos(angle);
+			t_mat[2][1] = 0;
+			t_mat[2][2] = 1;
 		}else if(trans==3){
-			printf("Enter Tx and Sy : ");
+			printf("Enter Tx and Ty : ");
 			scanf("%d%d", &tx, &ty);
 			t_mat[0][0] = 1;
 			t_mat[1][1] = 1;
@@ -91,7 +98,10 @@ void main(){
 	}else if(type==2){
 		rectangle(result[0][0], result[1][0], result[0][1], result[1][1]);
 	}else if(type==3){
-		circle(result[0][0], result[1][0], rad);
+		if(trans!=1)
+			circle(result[0][0], result[1][0], rad);
+		else
+			circle(x1+rad*sx, y1+rad*sy, rad);
 	}else{
 		printf("Invalid Input\n");
 	}
